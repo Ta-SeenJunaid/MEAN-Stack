0e3a68e1198dcd582ecafab625fd42b39b54b877
Ta-SeenJunaid <taseen.junaid@gmail.com>
2022-09-16 16:41:48 +0200

    Components and props

--- react_refresher/my-app/src/App.js
+++ react_refresher/my-app/src/App.js
@@ -1,7 +1,20 @@
 import React from 'react';
 
+import GoalList from './components/GoalList'
+import './App.css'
+
 const App = () => {
-  return <h1>Hello from React!!!!!!!</h1>
+  const courseGoals = [
+    {id: '1', text: 'Complete the course'},
+    {id: '2', text: 'Practice a lot'},
+    {id: '3', text: 'Understand properly'}
+  ];
+  return (
+    <div className="course-goals">
+      <h2>Course Goals</h2>
+      <GoalList goals={courseGoals}/>
+    </div>
+  );
 };
 
 export default App;
