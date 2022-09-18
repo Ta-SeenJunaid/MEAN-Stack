d797b2a5fcda2b0006928377ba15a363e493bf35
Ta-SeenJunaid <taseen.junaid@gmail.com>
2022-09-18 14:35:10 +0200

    React state, hooks & completion of react refresher

--- react_refresher/my-app/src/App.js
+++ react_refresher/my-app/src/App.js
@@ -1,17 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import GoalList from './components/GoalList'
+import GoalList from './components/GoalList/GoalList'
+import NewGoal from './components/NewGoal/NewGoal'
 import './App.css'
 
 const App = () => {
-  const courseGoals = [
+  const [courseGoals, setCourseGoals] = useState([
     {id: '1', text: 'Complete the course'},
     {id: '2', text: 'Practice a lot'},
     {id: '3', text: 'Understand properly'}
-  ];
+  ]);
+
+  const addNewGoalHandler = newGoal => {
+    //setCourseGoals(courseGoals.concat(newGoal));
+    setCourseGoals(prevCourseGoals =>  prevCourseGoals.concat(newGoal));
+  };
+
   return (
     <div className="course-goals">
       <h2>Course Goals</h2>
+      <NewGoal onAddGoal={addNewGoalHandler}/>
       <GoalList goals={courseGoals}/>
     </div>
   );
